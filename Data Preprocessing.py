7969e1a2924bb9c2b25697dd5ec5c5173e0115dd
larswrede <lars.wrede@fs-students.de>
2022-02-21 17:03:07 +0100

    Import Data

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -23,28 +23,9 @@ for s in stock_data['Symbol']: # iterate for every stock indices
     # Retrieve data from Yahoo Finance
     tickerData = yf.Ticker(s)
     # Save historical data 
-    stock_dict[s] = tickerData.history(period='1d', start='2021-1-1', end='2021-12-31')
-# Concatenate all data
-stocks_as_df = pd.concat(stock_list, axis = 0)
-
-''' Pulls time series data for stocks on a daily basis from 2021-1-1 until 2021-12-31.
-Parameters
-----------
-:stock_dict:  dictionary
-    Contains the stock symbols as key and the time series as values.
-:stocks_as_df:  dataframe
-    Contains the time series data as one df.
--------
-'''
-
-stock_dict = {}
-for s in stock_data['Symbol']: # iterate for every stock indices
-    # Retrieve data from Yahoo Finance
-    tickerData = yf.Ticker(s)
-    # Save historical data 
     stock_dict[s] = yf.download(s, start='2021-1-1', end='2021-12-31', progress=False)
 # Concatenate all data
-stocks_as_df = pd.concat(stock_list, axis = 0)
+stocks_as_df = pd.concat(stock_dict, axis = 0)
 
 ''' Transform daily data to weekly 
 Parameters
@@ -55,4 +36,4 @@ Parameters
 '''
 stock_weekly = {}
 for s in stock_data['Symbol']:
-    stock_weekly[s] = stock_dict[s['Adj Close'].resample("W").mean()
+    stock_weekly[s] = stock_dict[s]['Adj Close'].resample("W").mean()
