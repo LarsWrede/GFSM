bd7c0a4723c292bcc65ccb55998bd10ce2668d6f
Nicolas Kepper <nicolas.kepper@gmail.com>
2022-03-13 15:54:25 +0100

    Adapt everything to new structure
    all calculations work now

--- 02_Analyses/02_price_pressure.py
+++ 02_Analyses/02_price_pressure.py
@@ -15,15 +15,17 @@ def prep_dfs():
     :return: stockdata volume, stockdata close, info
     """
     stocks = pd.read_csv(git_uri + delim + '01_Data_and_Preprocessing' + delim + 'stockdata_df.csv',
-                         parse_dates=['Date'], index_col='Date')#, dtype=np.float64)
+                         parse_dates=['Date'], index_col='Date')
     info = pd.read_csv(git_uri + delim + '01_Data_and_Preprocessing' + delim + 'info_df.csv',
                        parse_dates=['Announcement'])
     del info['Unnamed: 0']
 
-    vol = pd.DataFrame(data={col.split(" ")[0]: stocks[col] for col in stocks.columns if 'Volume' in col or col == 'Date'})
-    close = pd.DataFrame(data={col.split(" ")[0]: stocks[col] for col in stocks.columns if 'Close' in col or col == 'Date'})
+    vol = pd.DataFrame(data={col.split(" ")[0]: stocks[col] for col in stocks.columns if 'Volume' in col})
+    vol.insert(0, 'Date_Increment_ID', range(0, len(vol)))
+    close = pd.DataFrame(data={col.split(" ")[0]: stocks[col] for col in stocks.columns if 'Close' in col})
 
     return vol, close, info
+
 def calc_mvr_multiple_days(year_start, year_end, inclusions, day_range, stocks, market_symbol):
     """
     Calculates MVR measures and takes the average over multiple days.
@@ -50,7 +52,7 @@ def calc_mvr_multiple_days(year_start, year_end, inclusions, day_range, stocks, 
     s = pd.Series([], dtype='float64')
     for i in day_range:
         s_, _, _, n = calc_mvr(year_start, year_end, inclusions, i, stocks, market_symbol)
-        s.append(s_)
+        s = s.append(s_)
     return s, s.mean(), s.std(), n
 
 
@@ -84,14 +86,14 @@ def calc_mvr(year_start, year_end, inclusions, day, stocks, market_symbol):
     year_end = datetime.strptime('31.12.' + str(year_end), '%d.%m.%Y')
 
     inclusions_in_time_period = inclusions[
-        (inclusions['Aufgenommen'] >= year_start) & (inclusions['Aufgenommen'] <= year_end)]
+        (inclusions['Announcement'] >= year_start) & (inclusions['Announcement'] <= year_end)]
 
-    apply_calc_vr = lambda row: calc_vr(row['Aufgenommen'], day, stocks[row['Symbol']], stocks[market_symbol])
+    apply_calc_vr = lambda row: calc_vr(row['Announcement'], day, stocks, row['Ticker'], market_symbol)
     vr_series = inclusions_in_time_period.apply(apply_calc_vr, axis=1)
     return vr_series, vr_series.mean(), vr_series.std(), vr_series.size
 
 
-def calc_vr(announcement_date, day, stock, market):
+def calc_vr(announcement_date, day, stocks, stock_symbol, market_symbol):
     """
     VR of stock i is the ratio of Volume of stock i traded in period t to volume traded in the market times volume traded
         in the market over the past 8 weeks over volume of stock i in the past 8 weeks
@@ -108,10 +110,14 @@ def calc_vr(announcement_date, day, stock, market):
     """
 
     start_date = announcement_date - timedelta(weeks=8)
-    i = announcement_date + timedelta(days=day)
+    i = stocks[stocks.index == announcement_date]['Date_Increment_ID'].iloc[0] + day
+    i = stocks[stocks['Date_Increment_ID'] == i].index[0]
 
-    i_slice = stock[(stock.index >= start_date) & (stock.index <= i)]
-    m_slice = market[(market.index >= start_date) & (market.index <= i)]
+    stock = stocks[stock_symbol]
+    market = stocks[market_symbol]
+
+    i_slice = stock[(stock.index >= start_date) & (stock.index <= announcement_date)]
+    m_slice = market[(market.index >= start_date) & (market.index <= announcement_date)]
 
     v_it = stock[stock.index == i].values[0]
     v_mt = market[market.index == i].values[0]
@@ -119,12 +125,20 @@ def calc_vr(announcement_date, day, stock, market):
     v_i = i_slice.mean()
     v_m = m_slice.mean()
 
-    print(type(v_it), type(v_mt), type(v_i), type(v_m))
-
     return (v_it / v_mt) * (v_m / v_i)
 
 
 volumes, prices, info = prep_dfs()
 inclusions = info[info['Type'] == 'Included']
-print(volumes.dtypes)
-print(calc_vr(inclusions['Announcement'].iloc[0], 1, volumes['HEIG.DE'], volumes['.GDAXI']))
+
+#print(calc_vr(inclusions['Announcement'].iloc[0], 1, volumes, 'HEIG.DE', '.GDAXI'))
+#print(inclusions['Announcement'].iloc[0])
+
+print(calc_mvr(2010, 2012, inclusions, 1, volumes, '.GDAXI'))
+print(calc_mvr(2010, 2012, inclusions, 2, volumes, '.GDAXI'))
+print(calc_mvr(2010, 2012, inclusions, 3, volumes, '.GDAXI'))
+print(calc_mvr(2010, 2012, inclusions, 4, volumes, '.GDAXI'))
+print(calc_mvr(2010, 2012, inclusions, 5, volumes, '.GDAXI'))
+
+print(calc_mvr_multiple_days(2010, 2022, inclusions, range(1, 5), volumes, '.GDAXI'))
+
