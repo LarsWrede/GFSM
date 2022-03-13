1456f80cd4d9579d115c0884ebc149b768d7e8c8
dennisblaufuss <dennis_blaufuss@web.de>
2022-03-13 18:50:30 +0100

    corr result df

--- 02_Analyses/03_correlation.py
+++ 02_Analyses/03_correlation.py
@@ -10,14 +10,14 @@ unique_stocks = list(dict.fromkeys(list(info_df.loc[~info_df['Type'].isnull()]['
 corr_df_list = []
 for s in unique_stocks:
     for t in info_df.loc[~info_df['Type'].isnull()].loc[info_df['Ticker'] == s]['Type']:
-        temp_date = pd.to_datetime(info_df.loc[info_df['Ticker'] == s].loc[info_df['Type'] == t]['Date'].values[0])
+        temp_date = pd.to_datetime(info_df.loc[info_df['Ticker'] == s].loc[info_df['Type'] == t]['Date'].values[0])  # Change Date to Announcement if desired
         temp_data_before = stockdata_df[s + ' Return'].loc[temp_date - np.timedelta64(365, 'D'):temp_date]
         temp_data_after = stockdata_df[s + ' Return'].loc[temp_date:temp_date + np.timedelta64(365, 'D')]
         temp_dax_before = stockdata_df['.GDAXI Return'].loc[temp_date - np.timedelta64(365, 'D'):temp_date]
         temp_dax_after = stockdata_df['.GDAXI Return'].loc[temp_date:temp_date + np.timedelta64(365, 'D')]
         corr_before = temp_data_before.corr(temp_dax_before)
         corr_after = temp_data_after.corr(temp_dax_after)
-        if temp_date == np.datetime64('2021-09-20'):
+        if pd.to_datetime(info_df.loc[info_df['Ticker'] == s].loc[info_df['Type'] == t]['Date'].values[0]) == np.datetime64('2021-09-20'):
             big_inc = True
         else:
             big_inc = False
@@ -26,4 +26,12 @@ for s in unique_stocks:
 corr_df = pd.DataFrame(corr_df_list)
 corr_df.sort_values(by=['Delta'], inplace=True)
 
-print()
+big_inc_corr = corr_df.loc[corr_df['30 -> 40']]['Delta'].mean()
+rest_inc_corr = corr_df.loc[corr_df['Type'] == 'Included'].loc[corr_df['30 -> 40'] == False]['Delta'].mean()
+total_inc_corr = corr_df.loc[corr_df['Type'] == 'Included']['Delta'].mean()
+ex_corr = corr_df.loc[corr_df['Type'] == 'Excluded']['Delta'].mean()
+result_df = pd.DataFrame([total_inc_corr, big_inc_corr, rest_inc_corr,ex_corr])
+result_df = result_df.T.rename(index={0: 'Avg. Correlation'})
+result_df.columns = ['Included', 'Included 40', 'Included others', 'Excluded']
+
+print(result_df)
