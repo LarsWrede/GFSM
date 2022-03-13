9829a53aa5a87bb2679d5f37542a2c20ef8a1157
Nicolas Kepper <nicolas.kepper@gmail.com>
2022-03-13 11:32:00 +0100

    add nicolas git location

--- 01_Data & Preprocessing/01_preprocessing.py
+++ 01_Data & Preprocessing/01_preprocessing.py
@@ -15,8 +15,10 @@ if user == 'Lars':
     local_git_link = '/Users/dennisblaufuss/Desktop/Uni/Repos/GFSM'
 if user == 'sophiemerl':
     local_git_link = '/Users/sophiemerl/Desktop/GSFM/T10/GFSM'
-if user == 'Nicolas':
-    local_git_link = '/Users/dennisblaufuss/Desktop/Uni/Repos/GFSM'
+if user == 'nicol':
+    local_git_link = 'C:\\Users\\nicol\\git\\GFSM'
+if user == nicolaskepper:
+    local_git_link = '/Users/nicolaskepper/git/GFSM'
 if user == 'Phillip':  # lol
     local_git_link = '/Users/dennisblaufuss/Desktop/Uni/Repos/GFSM'
 
