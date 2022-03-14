5cf50a33485f45eef1d995c86f057fedf8d347c8
dennisblaufuss <dennis_blaufuss@web.de>
2022-03-14 19:23:41 +0100

    corr changes

--- 02_Analyses/03_correlation.py
+++ 02_Analyses/03_correlation.py
@@ -7,31 +7,65 @@ stockdata_df['Date'] = pd.to_datetime(stockdata_df['Date'])
 stockdata_df.set_index('Date', inplace=True)
 unique_stocks = list(dict.fromkeys(list(info_df.loc[~info_df['Type'].isnull()]['Ticker'])))
 
+
+def stock_corr(stock, type, datetype, timeframe):
+    if timeframe == 'year':
+        timedelta = np.timedelta64(365, 'D')
+    if timeframe == 'quarter':
+        timedelta = np.timedelta64(91, 'D')
+    if timeframe == 'month':
+        timedelta = np.timedelta64(30, 'D')
+    temp_date = pd.to_datetime(info_df.loc[info_df['Ticker'] == s].loc[info_df['Type'] == type][datetype].values[0])
+    temp_data_before = stockdata_df[stock + ' Return'].loc[temp_date - timedelta:temp_date]
+    temp_data_after = stockdata_df[stock + ' Return'].loc[temp_date:temp_date + timedelta]
+    temp_dax_before = stockdata_df['.GDAXI Return'].loc[temp_date - timedelta:temp_date]
+    temp_dax_after = stockdata_df['.GDAXI Return'].loc[temp_date:temp_date + timedelta]
+    corr_before = temp_data_before.corr(temp_dax_before)
+    corr_after = temp_data_after.corr(temp_dax_after)
+    return corr_before, corr_after
+
+
 corr_df_list = []
 for s in unique_stocks:
     for t in info_df.loc[~info_df['Type'].isnull()].loc[info_df['Ticker'] == s]['Type']:
-        temp_date = pd.to_datetime(info_df.loc[info_df['Ticker'] == s].loc[info_df['Type'] == t]['Date'].values[0])  # Change Date to Announcement if desired
-        temp_data_before = stockdata_df[s + ' Close'].loc[temp_date - np.timedelta64(365, 'D'):temp_date]
-        temp_data_after = stockdata_df[s + ' Close'].loc[temp_date:temp_date + np.timedelta64(365, 'D')]
-        temp_dax_before = stockdata_df['.GDAXI Close'].loc[temp_date - np.timedelta64(365, 'D'):temp_date]
-        temp_dax_after = stockdata_df['.GDAXI Close'].loc[temp_date:temp_date + np.timedelta64(365, 'D')]
-        corr_before = temp_data_before.corr(temp_dax_before)
-        corr_after = temp_data_after.corr(temp_dax_after)
+        cb_y_d, ca_y_d = stock_corr(s, t, 'Date', 'year')
+        cb_q_d, ca_q_d = stock_corr(s, t, 'Date', 'quarter')
+        cb_m_d, ca_m_d = stock_corr(s, t, 'Date', 'month')
+        cb_y_a, ca_y_a = stock_corr(s, t, 'Announcement', 'year')
+        cb_q_a, ca_q_a = stock_corr(s, t, 'Announcement', 'quarter')
+        cb_m_a, ca_m_a = stock_corr(s, t, 'Announcement', 'month')
         if pd.to_datetime(info_df.loc[info_df['Ticker'] == s].loc[info_df['Type'] == t]['Date'].values[0]) == np.datetime64('2021-09-20'):
             big_inc = True
         else:
             big_inc = False
-        corr_df_list.append({'Ticker': s, 'Type': t, '30 -> 40': big_inc, 'Corr_before': corr_before, 'Corr_after': corr_after, 'Delta': corr_after - corr_before})
+        corr_df_list.append({'Ticker': s, 'Type': t, '30 -> 40': big_inc
+                             , 'cb_y_d': cb_y_d, 'ca_y_d': ca_y_d, 'Delta_y_d': ca_y_d - cb_y_d
+                             , 'cb_q_d': cb_q_d, 'ca_q_d': ca_q_d, 'Delta_q_d': ca_q_d - cb_q_d
+                             , 'cb_m_d': cb_m_d, 'ca_m_d': ca_m_d, 'Delta_m_d': ca_m_d - cb_m_d
+                             , 'cb_y_a': cb_y_a, 'ca_y_a': ca_y_a, 'Delta_y_a': ca_y_a - cb_y_a
+                             , 'cb_q_a': cb_q_a, 'ca_q_a': ca_q_a, 'Delta_q_a': ca_q_a - cb_q_a
+                             , 'cb_m_a': cb_m_a, 'ca_m_a': ca_m_a, 'Delta_m_a': ca_m_a - cb_m_a
+                             })
 
 corr_df = pd.DataFrame(corr_df_list)
-corr_df.sort_values(by=['Delta'], inplace=True)
+# corr_df.sort_values(by=['Ticker'], inplace=True)
 
-big_inc_corr = corr_df.loc[corr_df['30 -> 40']]['Delta'].mean()
-rest_inc_corr = corr_df.loc[corr_df['Type'] == 'Included'].loc[corr_df['30 -> 40'] == False]['Delta'].mean()
-total_inc_corr = corr_df.loc[corr_df['Type'] == 'Included']['Delta'].mean()
-ex_corr = corr_df.loc[corr_df['Type'] == 'Excluded']['Delta'].mean()
-result_df = pd.DataFrame([total_inc_corr, big_inc_corr, rest_inc_corr,ex_corr])
-result_df = result_df.T.rename(index={0: 'Avg. Correlation'})
-result_df.columns = ['Included', 'Included 40', 'Included others', 'Excluded']
+result_df_list = []
+timeframes = ['year', 'quarter', 'month']
+types = ['date', 'announcement']
+for t in timeframes:
+    timeframe = t[:1]
+    for ty in types:
+        type = ty[:1]
+        big_inc_corr = corr_df.loc[corr_df['30 -> 40']]['Delta_' + timeframe + '_' + type].mean()
+        rest_inc_corr = corr_df.loc[corr_df['Type'] == 'Included'].loc[corr_df['30 -> 40'] == False]['Delta_' + timeframe + '_' + type].mean()
+        total_inc_corr = corr_df.loc[corr_df['Type'] == 'Included']['Delta_' + timeframe + '_' + type].mean()
+        ex_corr = corr_df.loc[corr_df['Type'] == 'Excluded']['Delta_' + timeframe + '_' + type].mean()
+        result_df_list.append({'Included': total_inc_corr, 'Included 40 ': big_inc_corr, 'Included Rest ': rest_inc_corr, 'Excluded ': ex_corr})
 
-print(result_df)
+result_df = pd.DataFrame(result_df_list)
+result_df = result_df.T
+result_df.columns = ['Avg. Corr year (Date)', 'Avg. Corr quarter (Date)', 'Avg. Corr month (Date)', 'Avg. Corr year (Announcement)', 'Avg. Corr quarter (Announcement)', 'Avg. Corr month (Announcement)']
+
+result_df.to_excel('corr_results.xlsx')
+# print(result_df)
